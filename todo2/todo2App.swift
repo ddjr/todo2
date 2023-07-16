5a2725229214bf79f881c9469992be9dc8380fa5
David Daly <ddalyjr@gmail.com>
2023-07-16 12:08:29 -0500

    Initial Commit

---
+++ todo2/todo2App.swift
@@ -0,0 +1,17 @@
+//
+//  todo2App.swift
+//  todo2
+//
+//  Created by David Daly on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct todo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
