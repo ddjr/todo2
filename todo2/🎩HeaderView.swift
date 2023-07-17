a833970ed4d9187abee1d67158f11f16f96a6d7c
David Daly <ddalyjr@gmail.com>
2023-07-17 18:42:34 -0400

    added header component

---
+++ todo2/🎩HeaderView.swift
@@ -0,0 +1,39 @@
+//
+//  🎩HeaderView.swift
+//  todo2
+//
+//  Created by David Daly on 7/17/23.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    let title: String
+    let subtitle: String
+    let angle: Double
+    let backgroundColor: Color
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 0)
+                .foregroundColor(backgroundColor)
+                .rotationEffect(Angle(degrees: angle))
+            VStack {
+                Text(title)
+                    .foregroundColor(.white)
+                    .font(.title)
+                Text(subtitle)
+                    .foregroundColor(.white)
+                    .font(.title2)
+            }
+        }
+        .frame(width: 600, height: 300)
+        .offset(y: -100)
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView(title: "Todo list", subtitle: "Get Things Done", angle: -15.0, backgroundColor: .pink)
+    }
+}
--- todo2/👮‍♂️LoginView.swift
+++ todo2/👮‍♂️LoginView.swift
@@ -15,58 +15,84 @@ import SwiftUI
 
 // MARK: 👀 View 👀
 struct LoginView: View {
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        🎩LoginHeader
-        🥷LoginForm
-        👨‍💻CreateAccountLink
+        
+        VStack {
+            // 🎩LoginHeader
+            HeaderView(
+                title: "Todo list",
+                subtitle: "Get Things Done",
+                angle: 15.0,
+                backgroundColor: .pink
+            )
+            
+            // 🥷LoginForm
+            Form {
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocorrectionDisabled()
+                    .autocorrectionDisabled()
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button() {
+                    // TODO: Add onClick
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10.0)
+                            .foregroundColor(.blue)
+                        
+                        Text("Login")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                }
+            }
+            
+            // 👨‍💻CreateAccountLink
+            VStack {
+                Text("New around here?")
+                NavigationLink("Create An Account",
+                               destination: RegisterView())
+                .foregroundColor(.blue)
+            }
+            .padding(.bottom, 50)
+            Spacer()
+        }
     }
 }
 
 // MARK: 🧩 Bits 🧩
-@ViewBuilder private var 🎩LoginHeader: some View {
-    ZStack {
-        RoundedRectangle(cornerRadius: 0)
-            .foregroundColor(.pink)
-        VStack {
-            Text("Todo List")
-                .foregroundColor(.white)
-                .font(.title)
-            Text("Get Things Done")
-                .foregroundColor(.white)
-                .font(.title2)
-        }
-    }
-    .frame(width: 100, height: 300)
-}
-
-@ViewBuilder private var 🥷LoginForm: some View {
-    @State var email = ""
-    @State var password = ""
-    Form {
-        TextField("Email Address", text: $email)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .autocorrectionDisabled()
-            .autocorrectionDisabled()
-        SecureField("Password", text: $password)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-        Button("Login") {
-            // TODO: Add onClick
-        }
-        .background(.blue)
-    }
-}
-
-@ViewBuilder private var 👨‍💻CreateAccountLink: some View {
-    VStack {
-        Text("New around here?")
-        NavigationLink("Create An Account",
-            destination: RegisterView())
-        .foregroundColor(.blue)
-    }
-    .padding(.bottom, 50)
-    Spacer()
-    
-}
+//@ViewBuilder private var 🎩LoginHeader: some View {
+//    ZStack {
+//        RoundedRectangle(cornerRadius: 0)
+//            .foregroundColor(.pink)
+//            .rotationEffect(Angle(degrees: 15))
+//        VStack {
+//            Text("Todo List")
+//                .foregroundColor(.white)
+//                .font(.title)
+//            Text("Get Things Done")
+//                .foregroundColor(.white)
+//                .font(.title2)
+//        }
+//    }
+//    .frame(width: 100, height: 300)
+//}
+//
+//@ViewBuilder private var 👨‍💻CreateAccountLink: some View {
+//    VStack {
+//        Text("New around here?")
+//        NavigationLink("Create An Account",
+//            destination: RegisterView())
+//        .foregroundColor(.blue)
+//    }
+//    .padding(.bottom, 50)
+//    Spacer()
+//
+//}
 
 // MARK: 🎥 Preview 🎥
 struct LoginView_Previews: PreviewProvider {
